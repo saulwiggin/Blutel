f65746fd90f669c0838606170e0114f67d4fac80
Saul Wiggin <saulwiggin@googlemail.com>
2014-10-06 08:54:59 +0100

    Foo Bar test

---
+++ sayhello.php
@@ -0,0 +1,37 @@
+<html>
+<head><title> BluTell interview questions</title></head>
+<body>
+// Ex. 1
+<b>
+<?php
+print "Hello, World!";
+?>
+</b>
+</body>
+
+<?php
+for ($x=17; $x<=53; $x++ {
+	if ($x/2 == 0) {
+		echo "Foo";
+		if ($x/5 == 0){
+			echo "Foo Bar";
+			} else {
+			echo $x;
+			}
+		} else {
+		echo $x;	
+			}
+	if ($x/5) == 0) {
+		echo "Bar";
+	} else {
+	echo $x;
+	}
+}
+		
+	
+	}
+?>
+
+//Ex.2 
+
+</html>
