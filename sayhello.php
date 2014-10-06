def864524163ea4d4b80b5da3306f9c486b13af8
Saul Wiggin <saulwiggin@googlemail.com>
2014-10-06 09:39:34 +0100

    using eclipse help

--- sayhello.php
+++ sayhello.php
@@ -1,9 +1,9 @@
 <html>
 <head><title> BluTell interview questions</title></head>
 <body>
-// Ex. 1
 <b>
 <?php
+// Ex. 1
 print "Hello, World!";
 ?>
 </b>
@@ -31,7 +31,47 @@ for ($x=17; $x<=53; $x++ {
 	
 	}
 ?>
-
+<?php
 //Ex.2 
+
+// customer id might change
+
+//EX3
+?>
+<form method="POST">
+	<select name="option">
+		<option value="0"> Please select ...</option>
+		<option value="a">Option 1</option>
+		<option value="b">Option 2</option>
+	</select>
+</form>
+
+<?php
+$option = $_POST["option"];
+switch($option){
+	case "0":
+		print "You didn't select an option !";
+		break;
+	case "a":
+		print "Option 1 selected";
+		break;
+	case "b":
+		print "Option 2 selected";
+		break;
+	}
+	?>
+
+<?php  
+
+//EX4.  Takes up space	
+
+//Ex5. due to the limitations of the relational database structure.
+
+//Ex6. 
+
+?>
+
+
+
 
 </html>
